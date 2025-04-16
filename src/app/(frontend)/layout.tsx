849bedcc0cf3216297c3cfb389c50fa342c82808
Theo Denanyoh <theo@theodenanyoh.com>
2025-04-16 11:24:43 +0000

    feat: add PostHog integration with rewrites for event tracking; update .gitignore to include .env.local and add PostHog dependencies in package.json

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -12,6 +12,7 @@ import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
+import { PostHogProvider } from '@/providers/PostHogProvider'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -27,17 +28,19 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
-        <Providers>
-          <AdminBar
-            adminBarProps={{
-              preview: isEnabled,
-            }}
-          />
+        <PostHogProvider>
+          <Providers>
+            <AdminBar
+              adminBarProps={{
+                preview: isEnabled,
+              }}
+            />
 
-          <Header />
-          {children}
-          <Footer />
-        </Providers>
+            <Header />
+            {children}
+            <Footer />
+          </Providers>
+        </PostHogProvider>
       </body>
     </html>
   )
